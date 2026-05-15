d07ebe6d494414036a96faac597078abf6c19b45
Tushar Gupta <tushargupta8954@gmail.com>
2026-05-15 15:02:59 +0530

    Connected to database

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config({ path: './Backend/.env' });
+console.log("ENV VALUE:", process.env.MONGODB_URI);
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
 
 const app = express();
 
