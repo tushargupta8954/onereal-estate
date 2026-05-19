fdc0a5794b4b76538a61ae1138ce3c1f05d2fec3
Tushar Gupta <tushargupta8954@gmail.com>
2026-05-19 16:26:58 +0530

    Created sign in api route

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js';
 import { errorHandler } from '../utils/error.js';
 import bcrypt from 'bcryptjs';  // ✅ import bcrypt
+import jwt from 'jsonwebtoken';  
 
 export const signup = async (req, res, next) => {  // ✅ added async
     try {
@@ -23,5 +24,21 @@ export const signup = async (req, res, next) => {  // ✅ added async
 
     } catch (error) {
         next(error);  // ✅ catches all errors
+    }
+};
+
+export const signin = async (req, res, next) => {  
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });  // ✅ find user by email
+        if (!validUser) return next(errorHandler(404, 'User not found'));  
+        const validPassword = bcrypt.compareSync(password, validUser.password);  // ✅ compare passwords
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '7d' });  
+        const { password: pass, ...rest} = validUser._doc;   // ✅ exclude password from response
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);  // ✅ set cookie and send user data
+        
+    } catch (error) {
+        next(error);
     }
 };
